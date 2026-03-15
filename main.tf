a2a56ba15e21e5c31e86d4473e7326a2ad4cbb6b
Yasminesaadouni <yasmine.saadouni@hec.ca>
2026-03-15 18:36:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 provider "azurerm" {
   features {}
-  use_cli = true
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "cr460-rg"
   location = "Canada Central"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "cr460-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
